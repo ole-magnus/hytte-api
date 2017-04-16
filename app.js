aabd23cd36a6ecfa5098f849d0b6c8443d76bef7
Ole-Magnus Aker <ole.magnus.aker@schibsted.com>
2017-04-16 17:14:52 +0200

    fixed images

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ var express = require('express')
 var app = express()
 var upload = require('./controller/upload')
 
+app.set('view engine', 'pug')
+app.use(express.static('uploads'))
+
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
@@ -29,6 +32,36 @@ app.get("/status", function(req, res) {
 	res.send("ON")
 	res.status(200).end()
 })
+
+
+app.get("/images", function(req, res) {
+	const imageFolder = './uploads/'
+	const fs = require('fs')
+
+	fs.readdir(imageFolder, (err, files) => {
+		const recordings = getRecordings(files)
+		res.render('index', { recordings: recordings })
+
+	})
+})
+
+function getRecordings(fileNames) {
+	const images = fileNames.filter(function(item) {
+		return item.endsWith('.jpg');
+	});
+	const recordings = images.map(function(fileName) {
+		const position = fileName.split('-')[0]
+		const time = fileName.split('-')[1].split('.')[0]
+		const recording = {
+			image: fileName,
+			position: position,
+			time: time
+		}
+		return recording
+	})
+	return recordings
+}
+
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
